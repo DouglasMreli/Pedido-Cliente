706062b60d72d06c3f452f783d10e5b5d1b34750
Douglas Mareli <82533319+DouglasMreli@users.noreply.github.com>
2021-08-22 20:03:23 -0300

    Update pedido.kt

--- pedido.kt
+++ pedido.kt
@@ -4,24 +4,17 @@ import java.io.FileWriter
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-/* FALTA CRIAR UMA FORMA DE VERIFICAR SE O CLIENTE JÁ EXISTE ATRAVES
-DO NOME E CRIAR O OBJETO PEDIDO
- */
-
 fun main() {
 
     val listaClientes = mutableListOf<Cliente>()
     val listaPedidos = mutableListOf<Pedido>()
 
-//    listaClientes.add(Cliente("Fulano", "Rua A", 9999))
-//    listaPedidos.add(Pedido(99999, 500.0, listaClientes[0]))
-//
-//    listaClientes.add(Cliente("Ciclano", "Rua B", 8888))
-//    listaPedidos.add(PedidoExpresso(88888, 500.0, listaClientes[1], LocalDateTime.now().plusDays(1)))
-
-
-    leArquivo(listaClientes, listaPedidos)
+    leArquivoCliente(listaClientes)
+    leArquivoPedido(listaClientes, listaPedidos)
+    gravaArquivos(listaClientes, listaPedidos)
     println(listaClientes)
+    println()
+    println(listaPedidos)
 }
 
 class Cliente(
@@ -35,7 +28,7 @@ class Cliente(
     }
 
     fun gravaEmArquivo(){
-        val fw = FileWriter(File("clientes-pedidos.txt"), true)
+        val fw = FileWriter(File("clientes.txt"), true)
         fw.append("cliente-$nome-$endereco-$telefone\n")
         fw.close()
     }
@@ -56,12 +49,12 @@ open class Pedido(
     override fun toString(): String {
         return "$cliente\n" +
                 "Preço do pedido: $preco\n" +
-                "Data e hora do pedido: $dataPedidoString}"
+                "Data e hora do pedido: $dataPedidoString"
 
     }
 
     open fun gravaEmArquivo(){
-        val fw = FileWriter(File("clientes-pedidos.txt"), true)
+        val fw = FileWriter(File("pedidos.txt"), true)
         fw.append("pedidonormal-$numero-$preco-$dataPedidoString-${cliente.getNome()}\n")
         fw.close()
     }
@@ -71,8 +64,10 @@ class PedidoExpresso(
     numero: Int,
     preco: Double,
     cliente: Cliente,
-    private val dataEntrega: LocalDateTime) : Pedido(numero, preco*1.2, cliente)
+    dataPedido: LocalDateTime,
+    private val dataEntrega: LocalDateTime) : Pedido(numero, preco*1.2, cliente, dataPedido)
 {
+    private val dataEntregaString = dataEntrega.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"))
 
     private fun entregueNoPrazo(): Boolean {
         return (dataEntrega.dayOfMonth == dataPedido.dayOfMonth) and
@@ -90,14 +85,14 @@ class PedidoExpresso(
     }
 
     override fun gravaEmArquivo(){
-        val fw = FileWriter(File("clientes-pedidos.txt"), true)
-        fw.append("pedidoexpresso-${numero}-$preco-$dataPedidoString-${cliente.getNome()}-${dataEntrega}\n")
+        val fw = FileWriter(File("pedidos.txt"), true)
+        fw.append("pedidoexpresso-${numero}-$preco-$dataPedidoString-${cliente.getNome()}-${dataEntregaString}\n")
         fw.close()
     }
 }
 
-fun leArquivo(listaClientes: MutableList<Cliente>, listaPedidos: MutableList<Pedido>) {
-    val fr = FileReader(File("clientes-pedidos.txt"))
+fun leArquivoCliente(listaClientes: MutableList<Cliente>) {
+    val fr = FileReader(File("clientes.txt"))
     val objetoEmString = fr.readLines()
     fr.close()
 
@@ -106,7 +101,7 @@ fun leArquivo(listaClientes: MutableList<Cliente>, listaPedidos: MutableList<Ped
 
             var indiceComeco = item.indexOf("-") + 1
             var indiceFim = item.indexOf(string = "-", startIndex = indiceComeco)
-            val nome = item.subSequence(8, indiceFim).toString()
+            val nome = item.subSequence(indiceComeco, indiceFim).toString()
 
             indiceComeco = indiceFim + 1
             indiceFim = item.indexOf(string = "-", startIndex = indiceComeco)
@@ -117,7 +112,17 @@ fun leArquivo(listaClientes: MutableList<Cliente>, listaPedidos: MutableList<Ped
             val numero = numeroString.toInt()
 
             listaClientes.add(Cliente(nome, endereco, numero))
-        }else if(item.startsWith("pedido")){
+        }
+    }
+}
+
+fun leArquivoPedido(listaClientes: MutableList<Cliente>, listaPedidos: MutableList<Pedido>) {
+    val fr = FileReader(File("pedidos.txt"))
+    val objetoEmString = fr.readLines()
+    fr.close()
+
+    for(item in objetoEmString) {
+        if (item.startsWith("pedido")) {
 
             var indiceComeco = item.indexOf("-") + 1
             var indiceFim = item.indexOf(string = "-", startIndex = indiceComeco)
@@ -127,29 +132,79 @@ fun leArquivo(listaClientes: MutableList<Cliente>, listaPedidos: MutableList<Ped
             indiceFim = item.indexOf(string = "-", startIndex = indiceComeco)
             val preco = (item.subSequence(indiceComeco, indiceFim).toString()).toDouble()
 
-
             indiceComeco = indiceFim + 1
             indiceFim = item.indexOf(string = "-", startIndex = indiceComeco)
-            val dataPedido = item.subSequence(indiceComeco, indiceFim).toString()
+            val dataPedidoString = item.subSequence(indiceComeco, indiceFim).toString()
+            val dataPedido: LocalDateTime = criaData(dataPedidoString)
 
             val nomeCliente: String
-            if(item.startsWith("pedidoexpresso")) {
+            if (item.startsWith("pedidoexpresso")) {
                 indiceComeco = indiceFim + 1
                 indiceFim = item.indexOf(string = "-", startIndex = indiceComeco)
                 nomeCliente = item.subSequence(indiceComeco, indiceFim).toString()
+                val cliente: Cliente = pegaCliente(listaClientes, nomeCliente)
 
                 indiceComeco = indiceFim + 1
-                val dataEntrega = (item.subSequence(indiceComeco, item.lastIndex + 1).toString())
-                //criar datas e o objeto
-            }else{
+                val dataEntregaString = (item.subSequence(indiceComeco, item.lastIndex + 1).toString())
+                val dataEntrega: LocalDateTime = criaData(dataEntregaString)
+
+                listaPedidos.add(PedidoExpresso(numeroPedido,preco,cliente,dataPedido,dataEntrega))
+            } else {
                 indiceComeco = indiceFim + 1
                 nomeCliente = item.subSequence(indiceComeco, item.lastIndex + 1).toString()
-                //criar objeto 
-            }
+                val cliente: Cliente = pegaCliente(listaClientes, nomeCliente)
 
+                listaPedidos.add(Pedido(numeroPedido,preco,cliente,dataPedido))
+            }
         }
     }
-
-
 }
 
+fun pegaCliente(listaClientes: MutableList<Cliente>, nomeCliente: String): Cliente {
+
+    for(cliente in listaClientes){
+        if (nomeCliente == cliente.getNome()){
+             return cliente
+        }
+    }
+     return Cliente(nomeCliente,"Não informado", 0)
+}
+
+fun criaData(dataString: String): LocalDateTime {
+
+    val dia = (dataString.subSequence(0, 2).toString()).toInt()
+
+    val mes = if(dataString[3] == '0'){
+        dataString[4].toString().toInt()
+    } else{
+        (dataString.subSequence(3, 5).toString()).toInt()
+    }
+
+    val ano = (dataString.subSequence(6, 10).toString()).toInt()
+
+    val hora = if(dataString[11] == '0'){
+        dataString[12].toString().toInt()
+    } else{
+        (dataString.subSequence(11, 13).toString()).toInt()
+    }
+
+    val minuto = if(dataString[14] == '0'){
+        dataString[15].toString().toInt()
+    } else{
+        (dataString.subSequence(14, 16).toString()).toInt()
+    }
+
+    return LocalDateTime.of(ano, mes, dia, hora, minuto)
+}
+
+
+fun gravaArquivos(listaClientes: MutableList<Cliente>, listaPedidos: MutableList<Pedido>){
+
+    for(cliente in listaClientes) {
+        cliente.gravaEmArquivo()
+    }
+
+    for(pedido in listaPedidos) {
+        pedido.gravaEmArquivo()
+    }
+}
